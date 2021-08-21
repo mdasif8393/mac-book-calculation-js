8d761a1c8b6cf9b6045e8a3f756ba49747393041
md.asif.8393 <md.asif.8393@gmail.com>
2021-08-21 10:05:26 +0600

    add js

---
+++ app.js
@@ -0,0 +1,52 @@
+function ramOptionChange(ram){
+    if(ram == 'eightGbRam'){
+        document.getElementById('ramPrice').innerText = 0;
+        totalPrice();
+    }
+
+    else{
+        document.getElementById('ramPrice').innerText = 180;
+        totalPrice();
+    }
+}
+
+function ssdOptionChange(ssd){
+    if(ssd == 'twoFiveSixSsd'){
+        document.getElementById('ssdPrice').innerText = 0;
+        totalPrice();
+    }
+    else if(ssd == 'fiveOneTwoSsd'){
+        document.getElementById('ssdPrice').innerText = 100;
+        totalPrice();
+    }
+    else{
+        document.getElementById('ssdPrice').innerText = 180;
+        totalPrice();
+    }
+}
+
+function deliveryOptionChange(delivery){
+    if(delivery == 'free'){
+        document.getElementById('deliveryPrice').innerText = 0;
+        totalPrice();
+    }
+
+    else{
+        document.getElementById('deliveryPrice').innerText = 20;
+        totalPrice();
+    }
+}
+
+function totalPrice(){
+    const basePrice = parseInt(document.getElementById('basePrice').innerText);
+    const ramPrice = parseInt(document.getElementById('ramPrice').innerText);
+    const ssdPrice = parseInt(document.getElementById('ssdPrice').innerText);
+    const deliveryPrice = parseInt(document.getElementById('deliveryPrice').innerText);
+    const totalPrice = basePrice + ramPrice + ssdPrice + deliveryPrice;
+    document.getElementById('subTotal').innerText = totalPrice;
+
+    
+}
+
+
+
